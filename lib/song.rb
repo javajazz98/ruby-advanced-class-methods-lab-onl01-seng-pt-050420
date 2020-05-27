e8b8e253628f73d79861abaae492f14787dca7c2
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-27 03:01:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,62 @@
 class Song
-  attr_accessor :name, :artist
-
+  attr_accessor :name, :artist_name
   @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
 
   def self.all
     @@all
   end
 
-  def artist_name
-    artist.name if artist
+  def save
+    self.class.all << self
+  end
+
+def self.create
+    song = self.new
+    @@all << song
+    song
   end
   
-  def self.new_by_filename(file)
-    name = file.gsub(".mp3","").split(" - ")
-    s = Song.new(name[1])
-    s.artist_name = name[0]
-    s
-  end  
-  
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
   end
-end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical()
+    @@all.sort_by{|x| x.name}
+  end
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+  
+  def self.destroy_all()
+    @@all.clear
+  end 
+end 
